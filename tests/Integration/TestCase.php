6a6eabc28254aac82b210988308d6cbf62d69b58
Alexandre D'Eschambeault <adeschambeault@exolnet.com>
2018-08-14 18:40:32 -0400

    Make the class Integration\TestCase abstract

--- tests/Integration/TestCase.php
+++ tests/Integration/TestCase.php
@@ -5,7 +5,7 @@ namespace Exolnet\Format\Tests\Integration;
 use Exolnet\Format\FormatServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
 
-class TestCase extends Orchestra
+abstract class TestCase extends Orchestra
 {
     /**
      * @param \Illuminate\Foundation\Application $app
